814e8c3ac48c48e93be0c72b3632d4d8f78edbae
JahnaviNanda <yjahnavi.sree@gmail.com>
2018-10-11 13:36:07 +0530

    Updated serverless

--- src/createprofile.js
+++ src/createprofile.js
@@ -2,28 +2,32 @@
 
 const AWS = require('aws-sdk'); 
 
+
+
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.insert = (event, context, callback) => {
+console.log("Request received :\n",JSON.stringify(event));
+console.log("Request received :\n",JSON.stringify(context));
 
-const insertCandidateP = candidate => {
-    console.log('Submitting candidate');
-    const candidateInfo = {
-      TableName: process.env.CANDIDATE_TABLE,
-      Item: candidate,
-    };
-    return dynamoDb.put(candidateInfo).promise()
-      .then(res => candidate);
-  };
-  
-  const candidateInfo = (fullname, email, experience) => {
-    const timestamp = new Date().getTime();
-    return {
-      fullname: fullname,
-      email: email,
-      experience: experience,
-      submittedAt: timestamp,
-      updatedAt: timestamp,
-    };
-  };
+const tableName = "";
+dynamoDb.putItem({
+    "TableName" : tableName,
+    "Item" : {
+        "name":{
+            "s":event.name
+        },
+        "id":{
+            "N":event.id
+        }
+    }
+},function(err,data){
+    if(err){
+        context.fail('hello......'+err)
+    } else {
+        console.log(JSON.stringify(data,null,''));
+    }
+}
+);
+
 }
